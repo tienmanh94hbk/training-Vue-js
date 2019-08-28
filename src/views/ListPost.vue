6ec6ee63cf0ebdf914cdb0f70a98a703f4ceee3e
Manh.Do <49017943+tienmanh94hbk@users.noreply.github.com>
2019-08-28 14:14:32 +0700

    #feature/1_vue_Pages hoan thanh bai 1.

---
+++ src/views/ListPost.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>LIST POST</p>
+    <button @click="movePage">Next</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      movePage: function () {
+        this.$router.push('/PostDetail').catch();
+      }
+    }
+  }
+</script>
