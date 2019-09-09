80416b1ba291685d11a21e8711e7e89da7ad0da3
Manh.Do <49017943+tienmanh94hbk@users.noreply.github.com>
2019-09-09 11:48:39 +0700

    #feature/5 changed passing data

--- src/views/PostDetail.vue
+++ src/views/PostDetail.vue
@@ -13,15 +13,11 @@
     export default {
         data() {
             return {
-                id: this.$route.params.id,
                 post: {}
             }
         },
         created() {
-            this.$http.get('https://jsonplaceholder.typicode.com/posts/' + this.id).then(function (data) {
-                console.log(data)
-                this.post = data.body;
-            });
+            this.post = this.$route.params.postDetail
         }
     }
 </script>
