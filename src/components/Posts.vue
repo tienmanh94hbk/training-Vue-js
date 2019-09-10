469d97e96591dd8b191214265e63870eedcf3437
Manh.Do <49017943+tienmanh94hbk@users.noreply.github.com>
2019-09-10 10:04:24 +0700

    #feature/4 fix cach hien thi component Post tren ListPost

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,51 +1,37 @@
-<template xmlns:data="http://www.w3.org/1999/xhtml">
-    <div class="home">
-        <h1>LIST POST</h1>
-        <div v-for="(post,index) in posts" :key="post.id" @click='sendData(post)'>
-            <div v-bind:id="index" v-if="post.id < 50" class="text-title"
-                 :class="{active: post.id %2===0, hide: post.id%2!==0}">
-                <h3>Id: {{ post.id }}- Title: {{ post.title }}</h3>
-            </div>
-        </div>
+<template>
+  <div >
+    <div @click='setClick(post)' :class="{active: post.id %2===0 && !post.clicked, hide: post.id%2!==0&& !post.clicked ,afterClick: post.clicked}">
+      <h3>Id: {{ post.id }}- Title: {{ post.title }}</h3>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                posts: [],
-                isActive: true,
-                isHide: false
-            }
-        },
-        mounted() {
-            this.$http.get('https://jsonplaceholder.typicode.com/posts')
-                .then(function (res) {
-                    this.posts = res.body;
-                }).catch(function (error) {
-                console.log('Error: ', error);
-            })
-        },
-        methods: {
-            sendData(post) {
-                this.$router.push({
-                    name: 'PostDetail',
-                    params: {
-                        postDetail: post
-                    }
-                });
-            }
-        }
-    }
+  export default {
+    props:['post'],
+    methods: {
+      setClick(post) {
+        post.clicked = true;
+        this.$router.push({
+          name: 'PostDetail',
+          params: {
+            postDetail: post
+          }
+        });
+      }
+    },
+  }
 </script>
 
 <style scoped>
-    .active {
-        background-color: yellow;
-    }
+  .active {
+    background-color: yellow;
+  }
 
-    .hide {
-        background-color: green;
-    }
+  .hide {
+    background-color: green;
+  }
+  .afterClick{
+    background-color: gray;
+  }
 </style>
--- src/views/ListPost.vue
+++ src/views/ListPost.vue
@@ -1,19 +1,34 @@
 <template>
-    <div>
-        <div>
-            <Post></Post>
-        </div>
-    </div>
+  <div>
+    <h1>LIST POST</h1>
+    <app-posts v-bind:post ="post" v-for="post in posts" :key="post.id" v-if="post.id < 50" class="text-title"></app-posts>
+  </div>
 </template>
 
 <script>
-    import Post from '../components/Posts'
-
-    export default {
-        components: {
-            Post,
-        },
+  import Post from '../components/Posts'
+  export default {
+    components: {
+      'app-posts': Post
+    },
+    created() {
+      this.$http.get('https://jsonplaceholder.typicode.com/posts')
+              .then(function (res) {
+                for (let i = 0; i < res.body.length; i++) {
+                  res.body[i].clicked = false
+                }
+                this.posts = res.body;
+              }).catch(function (error) {
+        console.log('Error: ', error);
+      })
+    },
+    data(){
+      return{
+        posts : []
+      }
     }
+  }
+
 </script>
 
 <style>
