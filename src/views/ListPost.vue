4bc12bef165b9904bce10266091eded913ad2a50
Manh.Do <49017943+tienmanh94hbk@users.noreply.github.com>
2019-09-11 16:18:03 +0700

    #feature/5 fix rules va thong bao loi cho cac input id,userId,title

--- src/views/ListPost.vue
+++ src/views/ListPost.vue
@@ -3,65 +3,49 @@
         <div>
             <button v-on:click="newPost()">New Post</button>
         </div>
-        <div class="home">
-            <h1>LIST POST</h1>
-            <div v-for="(post,index) in posts" :key="post.id" @click='sendData(post)'>
-                <div v-bind:id="index" v-if="post.id < 50" class="text-title"
-                     :class="{active: post.id %2===0, hide: post.id%2!==0}">
-                    <h3>Id: {{ post.id }}- Title: {{ post.title }}</h3>
-                </div>
-            </div>
-        </div>
+        <h1>LIST POST</h1>
+        <app-posts v-bind:post="post" v-for="post in posts" :key="post.id" v-if="post.id < 50"
+                   class="text-title"></app-posts>
     </div>
 </template>
 
 <script>
+    import Post from '../components/Posts'
 
     export default {
-        data() {
-            return {
-                posts: [],
-                isActive: true,
-                isHide: false
-            }
+        components: {
+            'app-posts': Post
         },
-        mounted() {
+        created() {
             this.$http.get('https://jsonplaceholder.typicode.com/posts')
                 .then(function (res) {
+                    for (let i = 0; i < res.body.length; i++) {
+                        res.body[i].clicked = false
+                    }
                     this.posts = res.body;
                 }).catch(function (error) {
                 console.log('Error: ', error);
-            }).then(()=>{
+            }).then(() => {
                 const objNewPost = this.$route.params.newPost;
                 if (objNewPost) {
                     this.posts.unshift(objNewPost);
                 }
             });
         },
+        data() {
+            return {
+                posts: []
+            }
+        },
         methods: {
             newPost() {
                 this.$router.push({
                     name: 'NewPost'
                 })
-            },
-            sendData(post) {
-                this.$router.push({
-                    name: 'PostDetail',
-                    params: {
-                        postDetail: post
-                    }
-                });
             }
         }
     }
 </script>
 
 <style>
-    .active {
-        background-color: yellow;
-    }
-
-    .hide {
-        background-color: green;
-    }
 </style>
